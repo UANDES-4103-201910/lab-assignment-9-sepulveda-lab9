9f5cb26f98f28a99f57514f4479af174f3e5989d
CamiBacan <cesepulveda@miuandes.cl>
2019-05-15 12:30:04 -0400

    some gems added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 	validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}, uniqueness: true
 
 	validates :phone, length: {minimum: 9, maximum: 12}, allow_blank: true
-  	validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "assword must be between 8 to 12 alphanumeric characters"}
+  	validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "password must be between 8 to 12 alphanumeric characters"}
 
 
 
